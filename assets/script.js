9c8b58421514f354ac4f17105b314eb6197fea0d
CKYKA513 <90738854+CKYKA513@users.noreply.github.com>
2022-10-16 10:46:29 -0400

    fonctionnalité

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,12 @@
 // videos
 
 let videos = document.querySelectorAll('video');
-let progres = document.querySelector('.progres');
-let barres = document.querySelectorAll('.barre');
+let videoIl = document.querySelector('.video_il')
+let videoManiac = document.querySelector('.video_maniac')
+let progres1 = document.querySelector('.progres1');
+let barre1 = document.querySelector('.barre1');
+let progres2 = document.querySelector('.progres2')
+let barre2 = document.querySelector('.barre2')
 
 videos.forEach(video => {
   video.addEventListener("click", () => {
@@ -12,19 +16,73 @@ videos.forEach(video => {
       video.play()
     }
   })
-  video.addEventListener('timeupdate', function() {
-    barres.forEach(barre => {
-      let ratio = video.currentTime/video.duration;
-    console.log(ratio);
-    barre.style.transform = `scaleX(${ratio})`;
-    })
-    
-  })
+  if (video.currentTime == video.duration) {
+    video.currentTime = 0
+    video.pause()
+  }
+})
+
+videoIl.addEventListener('timeupdate', function() {
+  let ratio = videoIl.currentTime/videoIl.duration;
+  console.log(ratio);
+  barre1.style.transform = `scaleX(${ratio})`;
+  
+})
+
+videoManiac.addEventListener('timeupdate', function() {
+  let ratio = videoManiac.currentTime/videoManiac.duration;
+  console.log(ratio);
+  barre2.style.transform = `scaleX(${ratio})`;
+  
 })
 
 // sonore
 
-let playBtn = document.querySelector
+let playBtn1 = document.querySelector('.playBtn1')
+let stopBtn1 = document.querySelector('.stopBtn1')
+let playBtn2 = document.querySelector('.playBtn2')
+let stopBtn2 = document.querySelector('.stopBtn2')
+let montage = document.querySelector('.montage_audio')
+let tempsReel = document.querySelector('.tempsreel_audio')
+
+stopBtn1.style.display = "none"
+stopBtn2.style.display = "none"
+
+playBtn1.addEventListener("click", () => {
+  montage.play()
+  playBtn1.style.display = "none"
+  stopBtn1.style.display = "block"
+})
+stopBtn1.addEventListener("click", () => {
+  montage.pause()
+  stopBtn1.style.display = "none"
+  playBtn1.style.display = "block"
+})
+
+if (montage.currentTime == montage.duration) {
+  montage.currentTime = 0
+  montage.pause()
+  stopBtn1.style.display = "none"
+  playBtn1.style.display = "block"
+}
+
+playBtn2.addEventListener("click", () => {
+  tempsReel.play()
+  playBtn2.style.display = "none"
+  stopBtn2.style.display = "block"
+})
+stopBtn2.addEventListener("click", () => {
+  tempsReel.pause()
+  stopBtn2.style.display = "none"
+  playBtn2.style.display = "block"
+})
+
+if (tempsReel.currentTime == tempsReel.duration) {
+  tempsReel.currentTime = 0
+  tempsReel.pause()
+  stopBtn2.style.display = "none"
+  playBtn2.style.display = "block"
+}
 
 // slider
 
