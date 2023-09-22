2c1ac8fe31a47c6e0b65da430fcc94580f216da3
CKYKA513 <d.robberts911@gmail.com>
2023-09-22 13:12:46 -0400

    refonte menu + ajout page html
    création menu drop down pour le btn projet du menu
    ajout de tag de méta donné dans le head
    création de pages pour les différent types de projets + page de contact
    ajout du menu dans les différentes pages

---
+++ assets/menuScript.js
@@ -0,0 +1,57 @@
+// Header
+
+window.addEventListener("scroll", () => {
+    let header = document.querySelector(".header")
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop
+    let nom = document.querySelector(".nom")
+    let windowWidth = window.innerWidth;
+    let menu = document.querySelector(".menu")
+  
+    if (windowWidth >= 1500) {
+      if (scrollTop > 0) {
+        header.style.height = '5%'
+        nom.style.fontSize = '2vw'
+      } else {
+        header.style.height = '20%'
+        nom.style.fontSize = '5vw'
+        // menu.style.transform = "translateY(50px)" 
+      }
+    } else if (windowWidth >= 1200) {
+      if (scrollTop > 0) {
+        header.style.height = '5%'
+        nom.style.fontSize = '2vw'
+      } else {
+        header.style.height = '7%'
+        nom.style.fontSize = '5vw'
+      }
+    } else if (windowWidth >= 900) {
+      if (scrollTop > 0) {
+        header.style.height = '5%'
+        nom.style.fontSize = '2vw'
+      } else {
+        header.style.height = '4%'
+        nom.style.fontSize = '5vw'
+      }
+    }
+    
+  })
+
+// drop down menu
+
+// Get the dropdown button and content
+let dropdownButton = document.querySelector(".dropBtnProjet");
+let dropdownContent = document.querySelector(".dropContent");
+
+// Toggle the dropdown when the button is clicked
+dropdownButton.addEventListener("click", () => {
+    dropdownContent.style.display = (dropdownContent.style.display === "block") ? "none" : "block";
+});
+
+// Close the dropdown if the user clicks outside of it
+window.addEventListener("click", (event) => {
+    if (!event.target.matches(".dropBtnProjet")) {
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        }
+    }
+});
